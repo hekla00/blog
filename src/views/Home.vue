9d2e6285c048d7d099c549ef183e20fb0be29039
Hekla Alexía Samúelsdóttir <hekla@villingaholt.nu>
2022-01-04 18:16:56 +0100

    Lesson 76: Using watch and watchEffect

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,11 +4,12 @@
     <input type="text" v-model="search" />
     <p>search term - {{ search }}</p>
     <div v-for="name in matchingNames" :key="name">{{ name }}</div>
+    <button @click="handleClick">stop watching</button>
   </div>
 </template>
 
 <script>
-import { computed, ref } from "vue";
+import { computed, ref, watch, watchEffect } from "vue";
 export default {
   name: "Home",
   setup() {
@@ -22,11 +23,25 @@ export default {
       "brian",
     ]);
 
+    // to stop the watch and watchEffect function assign them to a variable and invoke them
+    const stopWatch = watch(search, () => {
+      console.log("watch function ran");
+    });
+
+    const stopEffect = watchEffect(() => {
+      console.log("watchEffect function ran", search.value);
+    });
+
     const matchingNames = computed(() => {
       return names.value.filter((name) => name.includes(search.value));
     });
 
-    return { names, search, matchingNames };
+    const handleClcik = () => {
+      // here you are invoking the stop watch function
+      stopWatch(), stopEffect();
+    };
+
+    return { names, search, matchingNames, handleClcik };
   },
 };
 </script>
