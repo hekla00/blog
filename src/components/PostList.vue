ba4a6a22f6a0e72af05ba551f9f718c366756fb7
Hekla Alexía Samúelsdóttir <hekla@villingaholt.nu>
2022-01-04 19:43:09 +0100

    Lesson 78: Lifecycle Hooks

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -7,11 +7,22 @@
 </template>
 
 <script>
+import { onMounted, onUnmounted, onUpdated } from "vue";
 import SinglePost from "../components/SinglePost.vue";
 export default {
   props: ["posts"],
   components: { SinglePost },
-  setup(props) {},
+  setup(props) {
+    onMounted(() => {
+      console.log("component mounted");
+    });
+    onUnmounted(() => {
+      console.log("component unmounted");
+    });
+    onUpdated(() => {
+      console.log("component updated");
+    });
+  },
 };
 </script>
 
