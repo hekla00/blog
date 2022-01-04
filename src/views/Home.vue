238b087efe04baeac3ae9b5e76c89f1cab474615
Hekla Alexía Samúelsdóttir <hekla@villingaholt.nu>
2022-01-04 18:08:46 +0100

    Lesson 75: Computed Values in the Setup Function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,37 +1,32 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <h2>Refs</h2>
-    <p>{{ userOne.name }} - {{ userOne.age }}</p>
-    <button @click="updateUserOne">Update User One</button>
-    <h2>Reactive</h2>
-    <p>{{ userTwo.name }} - {{ userTwo.age }}</p>
-    <button @click="updateUserTwo">Update User Two</button>
+    <input type="text" v-model="search" />
+    <p>search term - {{ search }}</p>
+    <div v-for="name in matchingNames" :key="name">{{ name }}</div>
   </div>
 </template>
 
 <script>
-import { ref, reactive } from "vue";
+import { computed, ref } from "vue";
 export default {
   name: "Home",
   setup() {
-    // When surrounding values with refs you make them reactive
-    const userOne = ref({ name: "Hekla", age: 21 });
-    // UserTwo is using reactive instead of using refs
-    const userTwo = reactive({ name: "Emma", age: 34 });
+    const search = ref("");
+    const names = ref([
+      "emma",
+      "annie",
+      "michelle",
+      "aaorn",
+      "connor",
+      "brian",
+    ]);
 
-    const updateUserOne = () => {
-      userOne.value.age = 25;
-    };
-    const updateUserTwo = () => {
-      userTwo.age = 38;
-    };
-    return { userOne, updateUserOne, userTwo, updateUserTwo };
-  },
-  data() {
-    return {
-      score: 5,
-    };
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value));
+    });
+
+    return { names, search, matchingNames };
   },
 };
 </script>
