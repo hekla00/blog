e06bf643ad725842136432ddcf7ed0fd793ba501
Hekla Alexía Samúelsdóttir <hekla@villingaholt.nu>
2022-01-04 22:59:36 +0100

    Lesson 88: useRoute

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -10,11 +10,14 @@
 <script>
 import getPost from "../composables/getPost";
 import Spinner from "../components/Spinner.vue";
+import { useRoute } from "vue-router";
 export default {
   props: ["id"],
   components: { Spinner },
   setup(props) {
-    const { error, post, load } = getPost(props.id);
+    const route = useRoute();
+
+    const { error, post, load } = getPost(route.params.id);
     load();
     return { error, post };
   },
