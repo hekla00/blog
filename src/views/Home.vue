46b208f71ced7af20a239513d5f6149fbc64ee6a
Hekla Alexía Samúelsdóttir <hekla@villingaholt.nu>
2022-01-04 23:47:11 +0100

    Lesson 90: Creating a Tag Cloud

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
     <div v-if="error">{{ error }}</div>
-    <div v-if="posts.length">
+    <div v-if="posts.length" class="layout">
       <PostList :posts="posts" />
+      <TagCloud :posts="posts" />
     </div>
     <div v-else>
       <Spinner />
@@ -17,9 +17,10 @@ import getPosts from "../composables/getPosts";
 // component imports
 import PostList from "../components/PostList.vue";
 import Spinner from "../components/Spinner.vue";
+import TagCloud from "../components/TagCloud.vue";
 export default {
   name: "Home",
-  components: { PostList, Spinner },
+  components: { PostList, Spinner, TagCloud },
   setup() {
     const { posts, error, load } = getPosts();
     load();
@@ -34,5 +35,10 @@ export default {
   max-width: 1200px;
   margin: 0 auto;
   padding: 10px;
+}
+.layout {
+  display: grid;
+  grid-template-columns: 3fr 1fr;
+  gap: 20px;
 }
 </style>
--- src/views/Tag.vue
+++ src/views/Tag.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tag">
     <div v-if="error">{{ error }}</div>
-    <div v-if="posts.length">
+    <div v-if="posts.length" class="layout">
       <PostList :posts="postsWithTag" />
     </div>
     <div v-else>
