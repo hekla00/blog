bbeb2f03a7a8e17bc67faf03a87cdd1960924239
Hekla Alexía Samúelsdóttir <hekla@villingaholt.nu>
2022-01-05 16:59:33 +0100

    Lesson 97: Deleting Documents

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -3,31 +3,37 @@
   <div v-if="post" class="post">
     <h3>{{ post.title }}</h3>
     <p class="pre">{{ post.body }}</p>
+    <button @click="handleClick" class="delete">delete post</button>
   </div>
-  <div v-else><Spinner /></div>
+  <div v-else>
+    <Spinner />
+  </div>
 </template>
 
 <script>
 import getPost from "../composables/getPost";
+import { useRoute, useRouter } from "vue-router";
+import { projectFirestore } from "../firebase/config";
+// component imports
 import Spinner from "../components/Spinner.vue";
-import { useRoute } from "vue-router";
 export default {
   props: ["id"],
   components: { Spinner },
   setup(props) {
     const route = useRoute();
-
     const { error, post, load } = getPost(route.params.id);
+    const router = useRouter();
     load();
-    return { error, post };
+    const handleClick = async () => {
+      await projectFirestore.collection("posts").doc(props.id).delete();
+      router.push({ name: "Home" });
+    };
+    return { error, post, handleClick };
   },
 };
 </script>
 
 <style scoped>
-.tags a {
-  margin-right: 10px;
-}
 .post {
   max-width: 1200px;
   margin: 0 auto;
@@ -39,5 +45,8 @@ export default {
 }
 .pre {
   white-space: pre-wrap;
+}
+button.delete {
+  margin: 10px auto;
 }
 </style>
